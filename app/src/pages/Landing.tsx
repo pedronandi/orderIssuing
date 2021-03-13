d901259ef191370d04a3934101fb137d1f7a1d32
Pedro Nandi <pedronandi@gmail.com>
2021-03-13 09:39:49 -0300

    Creating the landing page

---
+++ app/src/pages/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../styles/pages/landing.css';
+
+function Landing() {
+    return (
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <main>
+                <h1>Emissão de Pedidos</h1>
+                <p>Abaixo, os pedidos já existentes:</p>
+                </main>
+            </div>
+        </div>
+    ); 
+}
+
+export default Landing;
